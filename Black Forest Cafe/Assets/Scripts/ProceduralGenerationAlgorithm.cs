f82328c7376362b00554fe250aab6715242cad9d
Patticatti <139997703+Patticatti@users.noreply.github.com>
2023-07-24 18:01:08 -0700

    p15

--- Black Forest Cafe/Assets/Scripts/ProceduralGenerationAlgorithm.cs
+++ Black Forest Cafe/Assets/Scripts/ProceduralGenerationAlgorithm.cs
@@ -71,7 +71,7 @@ public static class ProceduralGenerationAlgorithm
                     }
                     else if (room.size.y >= minHeight * 2)
                     {
-                        SplitHorizontally( minHeight, roomsQueue, room);
+                        SplitHorizontally(minHeight, roomsQueue, room);
                     }
                     else if (room.size.x >= minWidth && room.size.y >= minHeight) //cant be split but can contain room
                     {
@@ -86,21 +86,21 @@ public static class ProceduralGenerationAlgorithm
     private static void SplitVertically(int minWidth, Queue<BoundsInt> roomsQueue, BoundsInt room)
     {
         var xSplit = Random.Range(1, room.size.x);
-        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(xSplit, room.min.y, room.min.z);
+        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(xSplit, room.size.y, room.size.z));
         BoundsInt room2 = new BoundsInt(new Vector3Int(room.min.x + xSplit, room.min.y, room.min.z),
-            new Vector3Int(room.size.x - xSplit, room.size.y, room.size.z);
-        roomQueue.Enqueue(room1);
-        roomQueue.Enqueue(room2);
+            new Vector3Int(room.size.x - xSplit, room.size.y, room.size.z));
+        roomsQueue.Enqueue(room1);
+        roomsQueue.Enqueue(room2);
     }
 
     private static void SplitHorizontally(int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
     {
         var ySplit = Random.Range(1, room.size.y);
-        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(room.size.x, ySplit, room.min.z);
+        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(room.size.x, ySplit, room.min.z));
         BoundsInt room2 = new BoundsInt(new Vector3Int(room.min.x, room.min.y + ySplit, room.min.z),
-            new Vector3Int(room.size.x, room.size.y - ySplit, room.size.z);
-        roomQueue.Enqueue(room1);
-        roomQueue.Enqueue(room2);
+            new Vector3Int(room.size.x, room.size.y - ySplit, room.size.z));
+        roomsQueue.Enqueue(room1);
+        roomsQueue.Enqueue(room2);
     }
 }
 
