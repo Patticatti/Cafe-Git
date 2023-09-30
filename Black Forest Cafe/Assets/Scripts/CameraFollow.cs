9cbfae7cce34a610046599d02b5949cad01d605a
Patticatti <139997703+Patticatti@users.noreply.github.com>
2023-09-29 20:49:03 -0700

    fixed camra

--- Black Forest Cafe/Assets/Scripts/CameraFollow.cs
+++ Black Forest Cafe/Assets/Scripts/CameraFollow.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    //public float FollowSpeed = 2f;
+    public float FollowSpeed = 2f;
     public Transform target;
 
-    private void LateUpdate()
+    private void Update()
     {
+        //var newPos = target.position;
         transform.position = new Vector3(target.position.x, target.position.y, -10f);
         //transform.position = Vector3.Slerp(transform.position, newPos,FollowSpeed*Time.deltaTime);
     }
