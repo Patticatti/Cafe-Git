a1c705ad77749f51b6370c5a9fb3761df9d1c74b
Patticatti <139997703+Patticatti@users.noreply.github.com>
2024-05-19 17:26:58 -0700

    Added buncha stuff/removed stuff???

--- Black Forest Cafe/Assets/Scripts/Enemy/EnemyAttackScriptableObject.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "EnemyAttackScriptableObject", menuName = "ScriptableObjects/EnemyAttack")]
-public class EnemyAttackScriptableObject : ScriptableObject
-{
-    public float attackDamage = 20f;
-    public float attackRange = 10f;
-    //public AudioClip attacksound;
-}
--- Black Forest Cafe/Assets/Scripts/Enemy/EnemyScriptableObject.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "EnemyScriptableObject", menuName = "ScriptableObjects/Enemy")]
-public class EnemyScriptableObject : ScriptableObject
-{
-    public int health = 10;
-    public float speed = 3f;
-    public EnemyAttackScriptableObject enemyAttackType;
-}
